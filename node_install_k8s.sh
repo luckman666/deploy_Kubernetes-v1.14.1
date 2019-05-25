0e793b2eaab00ed5307f4705e72c9d8cd713fdeb
YangBo <b8_yang@163.com>
2019-05-25 21:38:48 +0800

    测试完成

---
+++ node_install_k8s.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+#b8_yang@163.com
+source ./base.config
+#masterip=${masterip}
+#hostname=${hostname}
+#k8s_version=${k8s_version}
+#hostip=${hostip}
+
+if [[ "$(whoami)" != "root" ]]; then
+	echo "please run this script as root ." >&2
+	exit 1
+fi
+
+log="./setup.log"  #操作日志存放路径 
+fsize=2000000         
+exec 2>>$log  #如果执行过程中有错误信息均输出到日志文件中
+
+echo -e "\033[31m 这个是centos7系统初始化脚本，请慎重运行！Please continue to enter or ctrl+C to cancel \033[0m"
+#sleep 5
+#yum update
+yum_update(){
+	yum update -y
+}
+#configure yum source
+yum_config(){
+  yum install wget epel-release -y
+  cd /etc/yum.repos.d/ && mkdir bak && mv -f *.repo bak/
+  wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+  wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+  yum clean all && yum makecache
+  yum -y install iotop iftop net-tools git yum-utils lrzsz expect gcc gcc-c++ make cmake libxml2-devel openssl-devel curl curl-devel unzip sudo ntp libaio-devel wget vim ncurses-devel autoconf automake zlib-devel  python-devel bash-completion
+}
+#firewalld
+iptables_config(){
+  systemctl stop firewalld.service
+  systemctl disable firewalld.service
+  #yum install iptables-services -y
+  #systemctl enable iptables
+  #systemctl start iptables
+  #iptables -F
+  #service iptables save
+  iptables -P FORWARD ACCEPT
+}
+#system config
+system_config(){
+  sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+  timedatectl set-local-rtc 1 && timedatectl set-timezone Asia/Shanghai
+  yum -y install chrony && systemctl start chronyd.service && systemctl enable chronyd.service
+}
+ulimit_config(){
+  echo "ulimit -SHn 102400" >> /etc/rc.local
+  cat >> /etc/security/limits.conf << EOF
+  *           soft   nofile       102400
+  *           hard   nofile       102400
+  *           soft   nproc        102400
+  *           hard   nproc        102400
+  *           soft  memlock      unlimited 
+  *           hard  memlock      unlimited
+EOF
+
+}
+
+ssh_config(){
+#sed -i "2i #bogeniubi\n#laoniubile" /etc/ssh/ssh_config
+#sed -i "s!#bogeniubi!StrictHostKeyChecking no!g" /etc/ssh/ssh_config
+#sed -i "s!#laoniubile!UserKnownHostsFile /dev/null!g" /etc/ssh/ssh_config
+if [`grep 'UserKnownHostsFile' /etc/ssh/ssh_config`];then
+echo "pass"
+else
+sed -i "2i StrictHostKeyChecking no\nUserKnownHostsFile /dev/null" /etc/ssh/ssh_config
+fi
+}
+
+#set sysctl
+sysctl_config(){
+  cp /etc/sysctl.conf /etc/sysctl.conf.bak
+  cat > /etc/sysctl.conf << EOF
+  #docker
+  net.bridge.bridge-nf-call-iptables = 1
+  net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+  /sbin/sysctl -p
+  echo "sysctl set OK!!"
+}
+
+#swapoff
+swapoff(){
+  /sbin/swapoff -a
+  sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+  echo "vm.swappiness=0" >> /etc/sysctl.conf
+  /sbin/sysctl -p
+}
+
+get_localip(){
+ipaddr='172.0.0.1'
+ipaddr=$(ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}')
+echo "$ipaddr"
+}
+
+setupkernel(){
+ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+ yum --enablerepo=elrepo-kernel install -y kernel-lt kernel-lt-devel
+ grub2-set-default 0
+}
+
+change_hosts(){
+num=0
+cd /root
+for host in $hostip
+do
+let num+=1
+if [ $host = `get_localip` ];then
+`hostnamectl set-hostname $hostname$num`
+fi
+done
+}
+
+
+#ssh trust
+rootssh_trust(){
+rm -rf ~/.ssh
+for host in `cat ./new_hostname_list.config`
+do
+if [ `hostname` != $host ];then
+#ls /root/.ssh
+if [ ! -f "/root/.ssh/id_rsa.pub" ];then
+expect ssh_trust_init.exp $root_passwd $host
+else
+expect ssh_trust_add.exp $root_passwd $host
+fi
+echo "remote machine root user succeed!!!!!!!!!!!!!!!! "
+fi
+done
+}
+
+ca_hash(){
+hash_value=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
+echo $hash_value
+}
+
+#install docker
+install_docker() {
+yum-config-manager --add-repo  https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum install -y --setopt=obsoletes=0 docker-ce-18.09.4-3.el7
+systemctl start docker
+systemctl enable docker
+}
+
+set_repo(){
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+	yum -y install kubelet-1.14.1 kubeadm-1.14.1 kubectl-1.14.1 kubernetes-cni-0.7.5
+	yum list installed | grep kube
+	systemctl daemon-reload
+	systemctl enable kubelet
+	systemctl start kubelet
+}
+
+install_masterk8s(){
+	images=(
+		kube-proxy:${k8s_version}
+        pause:3.1
+        )
+	for imagename in ${images[@]}; do
+	docker pull mirrorgooglecontainers/$imagename
+	docker tag mirrorgooglecontainers/$imagename k8s.gcr.io/$imagename
+	docker rmi mirrorgooglecontainers/$imagename
+	done
+	docker pull registry.cn-hangzhou.aliyuncs.com/openthings/k8s-gcr-io-coredns:1.3.1
+	docker tag registry.cn-hangzhou.aliyuncs.com/openthings/k8s-gcr-io-coredns:1.3.1 k8s.gcr.io/coredns:1.3.1
+	docker rmi registry.cn-hangzhou.aliyuncs.com/openthings/k8s-gcr-io-coredns:1.3.1
+	docker pull quay.io/coreos/flannel:v0.11.0-amd64
+}
+
+install_flannel(){
+	wget https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+	kubectl apply -f kube-flannel.yml
+}
+
+join_cluster(){
+ kubeadm join --token $tocken --discovery-token-ca-cert-hash sha256:$sha_value $masterip:6443
+}
+
+
+
+main(){
+ #yum_update
+ #setupkernel
+ yum_config
+ ssh_config
+ iptables_config
+ system_config
+ ulimit_config
+ sysctl_config
+ change_hosts
+ swapoff
+ install_docker
+ set_repo
+ 
+ rootssh_trust
+ 
+ install_masterk8s
+ install_flannel
+ 
+ join_cluster
+}
+main > ./setup.log 2>&1
